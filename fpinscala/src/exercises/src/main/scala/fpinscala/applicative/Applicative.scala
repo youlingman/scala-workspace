1eb6951ba247a55a1210ee0b61a9c2fe220934c2
youlingman <chenyoucyl@gmail.com>
2019-11-16 20:18:44 +0800

    emmm

--- fpinscala/src/exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ fpinscala/src/exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -208,8 +208,8 @@ trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
 
   def compose[G[_]](implicit G: Traverse[G]): Traverse[({type f[x] = F[G[x]]})#f] =
     new Traverse[({type f[x] = F[G[x]]})#f] {
-      override def traverse[M[_] : Applicative, A, B](fa: F[G[A]])(f: (A) => M[B]) = self
-//        self.traverse(fa)((ga: G[A]) => G.traverse(ga)(f))
+      override def traverse[M[_] : Applicative, A, B](fa: F[G[A]])(f: (A) => M[B]) =
+        self.traverse(fa)((ga: G[A]) => G.traverse(ga)(f))
     }
 }
 
